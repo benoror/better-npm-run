2996a4604254b0c9f49aa0d7f4bb8df387fa58ad
Goran Gajic <fero.null@gmail.com>
2015-11-07 17:45:55 +0100

    add simple test

---
+++ test.js
@@ -0,0 +1,9 @@
+
+
+if (process.env.TEST_ENV !== "TEST_VALUE") {
+    throw new Error(".env file is not loaded");
+}
+
+if (process.env.FOO !== "bar") {
+    throw new Error("env params are missing")
+}
