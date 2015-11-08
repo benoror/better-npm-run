edfa889710e16a899dee451cb49b701cfa3d6061
Goran Gajic <fero.null@gmail.com>
2015-11-08 10:30:10 +0100

    add ability to pass through CLI arguments #20

--- test.js
+++ test.js
@@ -7,3 +7,7 @@ if (process.env.TEST_ENV !== "TEST_VALUE") {
 if (process.env.FOO !== "bar") {
     throw new Error("env params are missing")
 }
+
+if (process.argv[2] !== "--test") {
+    throw new Error('it should accept params');
+}
